00cca54cc61b7dfe828a0a53d45f0f0f41ed4e30
bahat <bahat.omer@gmail.com>
2022-11-26 12:31:25 +0200

    added asciiArtPrint func

--- tool/AsciiArtTool.c
+++ tool/AsciiArtTool.c
@@ -4,3 +4,34 @@
 
 #include "AsciiArtTool.h"
 
+asciiArtPrintEncoded(RLEList list, FILE *out_stream)
+{
+    if(list==NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    const char* FOPEN_MODE = "w";
+    FILE* stream = fopen (out_stream, FOPEN_MODE);
+    if(stream==NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    char* listExportStatus;
+    char* encodedString = RLEListExportToString(list, listExportStatus);
+    assert(list!=NULL);
+    if(*listExportStatus!=RLE_LIST_SUCCESS)
+    {
+        fclose(stream);
+        return *listExportStatus;
+    }
+    int writeStatus = fputs(encodedString, stream);
+    free(encodedString);
+    fclose(stream);
+    if(writeStatus==EOF)
+    {
+        return RLE_LIST_ERROR;
+    }
+    return RLE_LIST_SUCCESS;
+    
+}
+
